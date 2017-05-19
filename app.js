fabf7b5eac0f66ed8efd6161fe8ff1c71609f8ca
Capi Etheriel <barraponto@gmail.com>
2017-05-19 16:45:35 -0300

    set up express-session

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
+var config = require('./config');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -20,6 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: config.SECRET, resave: true, saveUninitialized: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  SECRET: '4', // rolled dice, must be random
+}
