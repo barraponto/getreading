7539ebab589f743a302adddb8660d4334f0194f1
Capi Etheriel <barraponto@gmail.com>
2017-05-19 17:22:29 -0300

    added /users/signup form

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET user signup form */
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
 module.exports = router;
