b7ee37704b52873d8111f0afb65fc3acc199f3e4
Capi Etheriel <barraponto@gmail.com>
2017-05-20 13:55:42 -0300

    cheerio tests for /users/signup form

--- test/routes.js
+++ test/routes.js
@@ -2,10 +2,10 @@ const chai = require('chai');
 const chaiCheerio = require('chai-cheerio')
 const chaiHttp = require('chai-http');
 const cheerio = require('cheerio');
-const faker = require('faker');
 const app = require('../app');
 const mongoose = require('../mongoose');
 const config = require('../config');
+const mock = require('./mock');
 
 chai.use(chaiHttp);
 chai.use(chaiCheerio);
@@ -17,20 +17,20 @@ describe('Test forms', () => {
 
   it('should GET the /users/signup form', () =>
     chai.request(app).get('/users/signup')
-    .then((response) => {
-      response.statusCode.should.equal(200);
-      response.type.should.equal('text/html');
+      .then((response) => {
+        response.statusCode.should.equal(200);
+        response.type.should.equal('text/html');
 
-      const $ = cheerio.load(response.text);
-      $('form').should.exist;
-      $('form').should.have.attr('method').equal('POST');
-      ['email', 'password', 'repeat-password'].forEach((name) => {
-        $(`input[name="${name}"]`).should.exist;
-        $(`label[for="${name}"]`).should.exist;
-      });
-      $('input[name="password"]').should.have.attr('type').equal('password');
-      $('input[name="repeat-password"]').should.have.attr('type').equal('password');
-    })
+        const $ = cheerio.load(response.text);
+        $('form').should.exist;
+        $('form').should.have.attr('method').equal('POST');
+        ['email', 'password', 'repeat-password'].forEach((name) => {
+          $(`input[name="${name}"]`).should.exist;
+          $(`label[for="${name}"]`).should.exist;
+        });
+        $('input[name="password"]').should.have.attr('type').equal('password');
+        $('input[name="repeat-password"]').should.have.attr('type').equal('password');
+      })
   );
 
   after('Clear database', () => mongoose.connection.dropDatabase());
