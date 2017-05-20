80cc2a67cedd840e5fbdb949eaa33bc7370d5542
Capi Etheriel <barraponto@gmail.com>
2017-05-20 08:15:38 -0300

    test GET /users/signup form html

--- test/routes.js
+++ test/routes.js
@@ -1,11 +1,14 @@
 const chai = require('chai');
+const chaiCheerio = require('chai-cheerio')
 const chaiHttp = require('chai-http');
+const cheerio = require('cheerio');
 const faker = require('faker');
 const app = require('../app');
 const mongoose = require('../mongoose');
 const config = require('../config');
 
 chai.use(chaiHttp);
+chai.use(chaiCheerio);
 const should = chai.should();
 
 describe('Test forms', () => {
@@ -17,6 +20,16 @@ describe('Test forms', () => {
     .then((response) => {
       response.statusCode.should.equal(200);
       response.type.should.equal('text/html');
+
+      const $ = cheerio.load(response.text);
+      $('form').should.exist;
+      $('form').should.have.attr('method').equal('POST');
+      ['email', 'password', 'repeat-password'].forEach((name) => {
+        $(`input[name="${name}"]`).should.exist;
+        $(`label[for="${name}"]`).should.exist;
+      });
+      $('input[name="password"]').should.have.attr('type').equal('password');
+      $('input[name="repeat-password"]').should.have.attr('type').equal('password');
     })
   );
 
