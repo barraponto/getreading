224c740e58c3b4d25695a4f74d0dd10207b91153
Capi Etheriel <barraponto@gmail.com>
2017-05-20 13:57:05 -0300

    add confirmedFields middleware

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {User} = require('../models/user')
-const {requiredFields} = require('./utils');
+const {confirmedFields, requiredFields} = require('./utils');
 
 /* GET users listing. */
 router.get('/', (req, res) => {
@@ -14,13 +14,7 @@ router.get('/signup', (req, res) => res.render('signup'));
 /* POST new user from signup */
 router.post('/signup',
   requiredFields(['email', 'password', 'repeat-password']),
-  (req, res, next) => { // checks for proper passwords
-    if (req.body.password === req.body['repeat-password']){
-      next()
-    } else {
-      res.status(400).json({error: 'The repeated passwords do not match.'})
-    }
-  },
+  confirmedFields(['password', 'repeat-password']),
   (req, res) => { // actually creates the User
     User.create({email: req.body.email, password: req.body.password})
     .then((user) => res.json(user))
--- routes/utils.js
+++ routes/utils.js
@@ -8,7 +8,19 @@ const requiredFields = (fieldNames) => (req, res, next) => {
   }
 };
 
+const confirmedFields = (...pairs) => (req, res, next) => {
+  const unconfirmedFields = pairs.filter(
+    ([field, confirmedField]) => req.body[field] !== req.body[confirmedField]);
+  if (unconfirmedFields.length) {
+    const message = unconfirmedFields
+      .map(([field, unconfirmedField]) => `${unconfirmedField} has a different value from ${field}.`)
+      .join('\n');
+    res.status(400).json({error: message});
+  }
+  else { next(); }
+};
 
 module.exports = {
+  confirmedFields,
   requiredFields
 };
