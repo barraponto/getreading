15777bc117044ffe44eaca5146f6c593093b9ed1
Capi Etheriel <barraponto@gmail.com>
2017-05-20 07:34:24 -0300

    check password on a separate test

--- test/models.js
+++ test/models.js
@@ -5,24 +5,26 @@ const config = require('../config');
 const {User} = require('../models/user');
 
 const should = chai.should();
+const userData = {
+  email: faker.internet.email(),
+  password: faker.internet.password()
+};
 
 describe('Test model', () => {
   before('Connect mongoose', () => mongoose.connect(config.MONGODB_URL));
   before('Clear database', () => mongoose.connection.dropDatabase());
 
-  it('should save User and hash password', () => {
-    const userData = {
-      email: faker.internet.email(),
-      password: faker.internet.password()
-    };
+  it('should save User', () =>
+    User.create(userData).then((user) => {
+      user.email.should.be.a('string');
+      user.email.should.equal(userData.email);
+      userData.id = user.id;
+    })
+  );
 
-    return User.create(userData)
-      .then((user) => {
-        user.email.should.be.a('string');
-        user.email.should.equal(userData.email);
-        return user.checkPassword(userData.password);
-      })
-      .then((result) => result.should.be.true)
-  });
-
+  it('should check User pasword', () =>
+    User.findById(userData.id)
+      .then((user) => user.checkPassword(userData.password))
+      .then((result) => result.should.be.ok)
+  );
 });
