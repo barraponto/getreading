5392df92f0618cd7cda213b957266ed7348c25eb
Capi Etheriel <barraponto@gmail.com>
2017-09-30 14:02:14 -0300

    fix local strategy

--- passport.js
+++ passport.js
@@ -5,8 +5,8 @@ const {User} = require('./models/user');
 passport.use(new LocalStrategy(
   {usernameField: 'email', passwordField: 'password'},
   (email, password, done) => User.findOne({email})
-    .then((user) => (user) ? Promise.all([user, user.checkPassword(password)]) : done(null, false))
-    .then(([user, validated]) => (validated) ? done(null, user) : done(null, false))
+    .then((user) => Promise.all([user, user ? user.checkPassword(password) : false]))
+    .then(([user, validated]) => (user && validated) ? done(null, user) : done(null, false))
     .catch((err) => done(err, false))
 ));
 
